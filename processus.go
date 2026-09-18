5178ad0fcca0ce80889ad63b36e8ff46744800c4
Guillaume <guillaume.fontaine.ab@gmail.com>
2026-09-18 09:24:52 +0200

    parser json

--- processus.go
+++ processus.go
@@ -1,3 +1,48 @@
 package main
 
-// Les processus (Emitter, Parser, Killer) seront implémentés dans les étapes suivantes.
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Parser écoute les messages sur son channel in, valide leur format JSON
+// et renvoie la réponse (StatusOK ou StatusError) sur le channel out.
+func Parser(in <-chan Message, out chan<- Message) {
+	knownProcesses := make(map[string]bool)
+	fmt.Println("[PARSER] Démarrage du processus parser")
+
+	for msg := range in {
+		switch msg.Status {
+		case StatusNew:
+			knownProcesses[msg.Message] = true
+			fmt.Printf("[PARSER] Processus détecté : %s\n", msg.Message)
+
+		case StatusPut:
+			fmt.Printf("[PARSER] Message reçu : %s\n", msg.MessageID)
+
+			var data map[string]interface{}
+			err := json.Unmarshal([]byte(msg.Message), &data)
+
+			if err != nil {
+				fmt.Printf("[PARSER] Erreur de parsing JSON pour %s : %v\n", msg.MessageID, err)
+				out <- Message{
+					MessageID:    msg.MessageID,
+					Destinataire: "",
+					Status:       StatusError,
+					Message:      err.Error(),
+				}
+			} else {
+				fmt.Printf("[PARSER] JSON valide :\n%+v\n", data)
+				fmt.Printf("[PARSER] Envoi ACK %s\n", msg.MessageID)
+				out <- Message{
+					MessageID: msg.MessageID,
+					Status:    StatusOK,
+				}
+			}
+
+		case StatusKill:
+			fmt.Println("[PARSER] Signal kill reçu, arrêt du processus")
+			return
+		}
+	}
+}
