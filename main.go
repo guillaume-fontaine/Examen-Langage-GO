bad236e42a7e1c7cefffe292c9856591e82e78eb
Guillaume <guillaume.fontaine.ab@gmail.com>
2026-09-18 09:40:01 +0200

    implémentation dans main

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Initialisation du projet Message Queue")
+	fmt.Println("=== Démarrage du système Message Queue ===")
+
+	queue := NewQueue()
+
+	emitterIn := make(chan Message, 10)
+	parserIn := make(chan Message, 10)
+	killerIn := make(chan Message, 10)
+
+	// Lancer la Queue dans une goroutine
+	go queue.Run()
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	// Lancer les 3 processus concurrents
+	go func() {
+		defer wg.Done()
+		Emitter(emitterIn, queue.In)
+	}()
+
+	go func() {
+		defer wg.Done()
+		Parser(parserIn, queue.In)
+	}()
+
+	go func() {
+		defer wg.Done()
+		Killer(killerIn, queue.In)
+	}()
+
+	// Enregistrer les processus auprès de la Queue
+	queue.AddProcess("emitter", emitterIn)
+	queue.AddProcess("parser", parserIn)
+	queue.AddProcess("killer", killerIn)
+
+	// Attendre que le signal kill donné par le Killer provoque l'arrêt des goroutines
+	wg.Wait()
+
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("=== Arrêt complet du système Message Queue ===")
 }
